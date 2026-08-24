29745d7391d42c43abce96b6acf685fff5203981
Siba Mahankuda <sibamahankuda6@gmail.com>
2026-08-24 20:00:30 +0530

    add issueTypes file

---
+++ urbanpulse-frontend/src/constants/issueTypes.js
@@ -0,0 +1,11 @@
+export const ISSUE_TYPES = [
+  "Pothole",
+  "Garbage",
+  "Streetlight Failure",
+  "Water Leakage",
+  "Road Damage",
+  "Traffic Signal Failure",
+  "drainage problem",
+  "illegal Dumping "
+
+];
