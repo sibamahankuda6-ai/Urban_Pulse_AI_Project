513d98ac8e74544deedfcf9027a413d05e3443ca
Siba Mahankuda <sibamahankuda6@gmail.com>
2026-09-02 23:20:50 +0530

    added workers file

---
+++ urbanpulse-frontend/src/data/workers.js
@@ -0,0 +1,17 @@
+export const workers = [
+  {
+    id: 1,
+    name: "Ramesh",
+    department: "Roads",
+  },
+  {
+    id: 2,
+    name: "Suresh",
+    department: "Sanitation",
+  },
+  {
+    id: 3,
+    name: "Amit",
+    department: "Electricity",
+  },
+];
